e55509f11222c73334ace9494b93a6d55f2e2033
Mahmoudsafwt <99458514+Mahmoudsafwt@users.noreply.github.com>
2025-02-05 04:49:36 +0200

    Update cloudinary_config.js

--- src/utils/uploads/cloudinary_config.js
+++ src/utils/uploads/cloudinary_config.js
@@ -6,7 +6,7 @@ dotenv.config();
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRIT // Click 'View API Keys' above to copy your API secret
+    api_secret: process.env.API_SECRIT 
 });
 
 export default cloudinary;
